a914856481e80f7bb25e5f8275b2accfe4b20161
tiansrealm <tiansrealm@gmail.com>
2015-10-07 12:50:13 -0400

    image working

--- NYUCodebase/NYUCodebase/main.cpp
+++ NYUCodebase/NYUCodebase/main.cpp
@@ -4,8 +4,10 @@
 #include <SDL.h>
 #include <SDL_opengl.h>
 #include <SDL_image.h>
+
 #include "ShaderProgram.h"
 #include "Matrix.h"
+
 #ifdef _WINDOWS
 #define RESOURCE_FOLDER ""
 #else
@@ -13,6 +15,7 @@
 #endif
 
 SDL_Window* displayWindow;
+GLuint LoadTexture(const char *image_path);
 
 int main(int argc, char *argv[])
 {
@@ -20,41 +23,85 @@ int main(int argc, char *argv[])
 	displayWindow = SDL_CreateWindow("Tian's HW", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 360, SDL_WINDOW_OPENGL);
 	SDL_GLContext context = SDL_GL_CreateContext(displayWindow);
 	SDL_GL_MakeCurrent(displayWindow, context);
-	glViewport(0, 0, 640, 360);
-
-	ShaderProgram program (RESOURCE_FOLDER"vertex.glsl", RESOURCE_FOLDER"fragment.glsl");
-	/*Matrix projectionMatrix;
-	Matrix modelMatrix;
-	Matrix viewMatrix;
-	projectionMatrix.setOrthoProjection(-3.55f, 3.55f, -2.0f, 2.0f, -1.0f, 1.0f);*/
-
 #ifdef _WINDOWS
 	glewInit();
 #endif
+	glEnable(GL_BLEND);
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+	glViewport(0, 0, 640, 360);
+	ShaderProgram program (RESOURCE_FOLDER"vertex_textured.glsl", RESOURCE_FOLDER"fragment_textured.glsl");
 
+	Matrix projectionMatrix;
+	Matrix modelMatrix;
+	Matrix viewMatrix;
+	projectionMatrix.setOrthoProjection(-3.55f, 3.55f, -2.0f, 2.0f, -1.0f, 1.0f);
+
+	float lastFrameTicks = 0.0f;
+	GLuint backGroundTexture = LoadTexture("backgrounds.png");
 	SDL_Event event;
 	bool done = false;
 	while (!done) {
-		/*program.setModelMatrix(modelMatrix);
-		program.setProjectionMatrix(projectionMatrix);
-		program.setViewMatrix(viewMatrix);
-		glUseProgram(program.programID);
-		float vertices[] = { 0.5f, -0.5f, 0.0f, 0.5f, -0.5f, -0.5f };
-		glVertexAttribPointer(program.positionAttribute, 2, GL_FLOAT, false, 0, vertices);
-		glEnableVertexAttribArray(program.positionAttribute);
-		glDrawArrays(GL_TRIANGLES, 0, 3);
-		glDisableVertexAttribArray(program.positionAttribute);
-
-		modelMatrix.identity();*/
 		while (SDL_PollEvent(&event)) {
 			if (event.type == SDL_QUIT || event.type == SDL_WINDOWEVENT_CLOSE) {
 				done = true;
 			}
 		}
+		program.setModelMatrix(modelMatrix);
+		program.setProjectionMatrix(projectionMatrix);
+		program.setViewMatrix(viewMatrix);
+		glUseProgram(program.programID);
+		
+
+		modelMatrix.identity();
+		//modelMatrix.Scale(3.0f, 3.0f, 1.0f);
+		glClearColor(0.4f, 0.2f, 0.4f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
+
+		
+		
+
+		
+		float ticks = (float)SDL_GetTicks() / 1000.0f;
+		float elapsed = ticks - lastFrameTicks;
+		lastFrameTicks = ticks;
+
+		
+		float vertices[] = { -0.5f, -0.5f, 0.5f, 0.5f, -0.5f, 0.5f, 
+								0.5f, 0.5f, -0.5f, -0.5f, 0.5f, -0.5f };
+		float texCoords[] = { 0.0f, 1.0f, 1.0f, 0.0f, 0.0f, 0.0f, 
+								1.0f, 0.0f, 0.0f, 1.0f, 1.0f, 1.0f };
+
+		glVertexAttribPointer(program.positionAttribute, 2, GL_FLOAT, false, 0, vertices);
+		glEnableVertexAttribArray(program.positionAttribute);
+		glVertexAttribPointer(program.texCoordAttribute, 2, GL_FLOAT, false, 0, texCoords);
+		glEnableVertexAttribArray(program.texCoordAttribute);
+		glBindTexture(GL_TEXTURE_2D, backGroundTexture);
+		glDrawArrays(GL_TRIANGLES, 0, 6);
+		glDisableVertexAttribArray(program.texCoordAttribute);
+
 		SDL_GL_SwapWindow(displayWindow);
+
+		
+		
 	}
 
 	SDL_Quit();
 	return 0;
+}
+
+
+GLuint LoadTexture(const char *image_path){
+	SDL_Surface *surface = IMG_Load(image_path);
+	
+	GLuint textureID;
+	glGenTextures(1, &textureID);
+	glBindTexture(GL_TEXTURE_2D, textureID);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, surface->w, surface->h, 0, GL_RGB,
+		GL_UNSIGNED_BYTE, surface->pixels);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+	SDL_FreeSurface(surface);
+
+	return textureID;
 }
